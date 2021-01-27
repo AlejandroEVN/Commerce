9cf8b986fc28f0e7bb5e93af248cdd3e87beac15
Alejandro Noailles <alex24.11.88@gmail.com>
2021-01-27 17:01:38 +0000

    Delete network/static/network/JavaScript directory

--- network/static/network/JavaScript/newPost.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btnNewPost = document.querySelector('.btn-new-post');
-    const newPost = document.querySelector('.new-post');
-
-    btnNewPost.addEventListener('click', () => {
-        newPost.classList.toggle('show');
-    })
-})
